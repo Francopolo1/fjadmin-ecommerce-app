83e6f5e0af7e8b087ff7eece83811fec7126a21d
Frank Johnson <frankjohnson27@comcast.net>
2026-01-01 16:50:34 -0700

    remove invalid hostname

--- store/models.py
+++ store/models.py
@@ -51,8 +51,7 @@ class Customer(models.Model):
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
 
-class Product (models.Model):
-    id = models.IntegerField(primary_key=True)
+class Product (models.Model):    
     name = models.CharField(max_length=100)
     price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
