e5e272428ea6fd0ac9003fffb16c2bbae343f8f8
Frank Johnson <frankjohnson27@comcast.net>
2025-12-31 22:05:01 -0700

    Added db password

--- ecom/settings.py
+++ ecom/settings.py
@@ -120,15 +120,18 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 #    }
 #}
 
-import dj_database_url
-import os
+
 
 # ... other settings
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresgl',
+        'NAME':'railway',
+        'USER':'postgres',
+        'PASSWORD':os.environ['PGPASSWORD'],
+        'HOST':'turntable.proxy.rlwy.net',
+        'PORT':'54849',
     }
 }
 
